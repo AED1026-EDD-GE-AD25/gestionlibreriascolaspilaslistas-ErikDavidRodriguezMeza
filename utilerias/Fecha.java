6fb2ebb29bb159c32180d76dbcfa953b05fc5e5c
ErikDavidRodriguezMeza <L24170713@culiacan.tecnm.mx>
2025-10-25 17:37:53 +0000

    se agrego fecha usando codigo viejo

--- utilerias/Fecha.java
+++ utilerias/Fecha.java
@@ -1,7 +1,35 @@
 package utilerias;
 
 public class Fecha {
-	//coloca aquí el código visto en clase
 	
+    private int dia;
+    private int mes;
+    private int anio;
 
-} 
+  
+    public Fecha(int dia, int mes, int anio) {
+      
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+  
+    public String toString() {
+        // Use el String.format() de antiguo codigo para que dia, mes y año tengan
+		//dos digitos
+        return String.format("%02d/%02d/%d", dia, mes, anio);
+    }
+}
